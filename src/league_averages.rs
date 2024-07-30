a478ea5fc276a890bb02eaac983f3652be914ad8
joe <josephfielding03@gmail.com>
2024-07-30 01:54:36 -0400

    batting and pitching league averages

--- src/league_averages.rs
+++ src/league_averages.rs
@@ -1,45 +1,77 @@
-use std::ptr::replace;
-use regex::Regex;
-use term_table::row::Row;
-use term_table::{Table, TableStyle};
-use crate::query::QueryError;
+use regex::{Regex, RegexBuilder};
+use term_table::row::{Row};
+use term_table::{row, Table, TableStyle};
+use term_table::table_cell::{Alignment, TableCell};
+use crate::query::{empty, get_query_param, QueryError};
 
-pub(crate) fn display_league_batting_averages() -> Result<(), QueryError> {
-    let hitting_data = reqwest::blocking::get("https://www.baseball-reference.com/leagues/majors/bat.shtml")?.text()?;
+macro_rules! batting_url {
+    () => {
+        ("https://www.baseball-reference.com/leagues/majors/bat.shtml",
+        "Batting Stat Averages Per Team Per Game",
+        row!("Year", "G", "PA", "AB", "R", "H", "1B", "2B", "3B", "HR", "RBI", "SB", "CS", "BB",
+        "SO", "BA", "OBP", "SLG", "OPS", "TB", "GDP", "HBP", "SH", "SF", "IBB", "BIP"))
+    };
+}
+
+macro_rules! pitching_url {
+    () => {
+        ("https://www.baseball-reference.com/leagues/majors/pitch.shtml",
+        "Pitching Stat Averages Per Team Per Game",
+        row!("Year", "ERA", "G", "GF", "CG", "SHO", "tSHO", "SV", "IP", "H", "R", "ER", "HR",
+        "BB", "IBB", "SO", "HBP", "BK", "WP", "BF", "WHIP", "BAbip", "H9", "HR9", "BB9", "SO9", "SO/W", "E"))
+    };
+}
+
+pub(crate) fn display_league_averages(query: &Vec<String>, is_batting: bool) -> Result<(), QueryError> {
+    const ALL_TIME_INDEX: usize = 2;
+    const COL_OFFSET: usize = 4;
+
+    let (url, header, stat_header) = if is_batting { batting_url!() }
+    else { pitching_url!() };
+
+    let row_count = if get_query_param!(query, ALL_TIME_INDEX, empty!()) == "a" { -1 } else { 1 };
+
+    let stats = reqwest::blocking::get(url)?.text()?;
+
+    let table_regex = Regex::new(r"<tbody>[\S\s]*?League Year-By-Year").unwrap();
     let row_regex = Regex::new(r"<tr >(<th.*?</th>).*?</tr>").unwrap();
     let col_regex = Regex::new(r">([\d.]+)<").unwrap();
 
-    let mut matches = 0;
-    for cap in row_regex.captures_iter(hitting_data.as_str()) {
-        let row = &cap[0];
+    let mut table = Table::new();
+    table.style = TableStyle::thin();
+    table.add_row(stat_header.clone());
+    let mut rows = 0;
+
+    for row in row_regex.captures_iter(table_regex.find(stats.as_str()).unwrap().as_str()) {
+        let mut cols = vec![];
         let mut i = 0;
 
-        let mut table = Table::new();
-        table.style = TableStyle::thin();
-
-        let mut cols = vec![];
-        for col in col_regex.captures_iter(row) {
-            cols.push(col[1].to_string());
-            // println!("Res {i}: {}", &col[1]);
+        for col in col_regex.captures_iter(&row[0]) {
+            if i == 0 || i > COL_OFFSET {
+                cols.push(col[1].to_string());
+            }
             i += 1;
         }
-        table.add_row(Row::new(cols.clone()));
+
         table.add_row(Row::new(cols));
-        println!("{}", table.render().replace("│ ", "│")
-            .replace(" │", "│")
-            .replace("─┴─", "┴")
-            .replace("└─", "└")
-            .replace("─┘", "┘")
-            .replace("─┬─", "┬")
-            .replace("┌─", "┌")
-            .replace("─┐", "┐")
-            .replace("┼─", "┼")
-            .replace("─┼", "┼")
-            .replace("├─", "├")
-            .replace("─┤", "┤")
-        );
-        break;
+        rows += 1;
+        if rows == row_count { break };
+        if rows & 7 == 0 { table.add_row(stat_header.clone()) }
     }
-    println!("{matches}");
+
+    println!("\n{}\n{}", header, table.render()
+        .replace("│ ", "│")
+        .replace(" │", "│")
+        .replace("─┴─", "┴")
+        .replace("└─", "└")
+        .replace("─┘", "┘")
+        .replace("─┬─", "┬")
+        .replace("┌─", "┌")
+        .replace("─┐", "┐")
+        .replace("┼─", "┼")
+        .replace("─┼", "┼")
+        .replace("├─", "├")
+        .replace("─┤", "┤")
+    );
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,16 @@ mod query;
 mod league_averages;
 
 use std::{env};
-use std::process::exit;
 use crate::database::update_database;
 use crate::standings::display_standings;
 use crate::game::{games_query, season_games_query};
 use crate::leaders::display_leaders;
-use crate::league_averages::display_league_batting_averages;
+use crate::league_averages::display_league_averages;
 use crate::query::{empty, get_query_param};
 use crate::stats::{stats_query};
 use crate::teams::display_team_stats;
 
 fn main() {
-    display_league_batting_averages();
-    exit(0);
     const QUERY_TYPE_INDEX: usize = 1;
 
     let query: Vec<String> = env::args().collect();
@@ -31,9 +28,11 @@ fn main() {
         "g" => games_query(&query),
         "r" => season_games_query(&query),
         "u" => season_games_query(&query),
-        "p" => stats_query(&query),
+        "s" => stats_query(&query),
         "t" => display_team_stats(&query),
         "l" => display_leaders(&query),
+        "b" => display_league_averages(&query, true),
+        "p" => display_league_averages(&query, false),
         "update" => update_database(&query),
         _ => display_standings()
     };
